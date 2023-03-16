75516e3fc344043239d3405ef7c5859be1a519c1
syouzan420 <syouzanponkikki@yahoo.co.jp>
2023-03-16 11:41:45 +0900

    update

--- TeruFile.hs
+++ TeruFile.hs
@@ -1,16 +1,25 @@
-module TeruFile (fileRead, fileWrite) where
+module TeruFile (fileRead, fileWrite, fileRead2) where
 
-import System.IO(IOMode(..), openFile, hClose, hGetContents', hSetEncoding, utf8, hPutStr)
+import System.IO(FilePath, IOMode(..), openFile, hClose, hGetContents', hSetEncoding
+                , utf8, hPutStr)
+import Data.Text.Encoding (decodeUtf8)
+import Data.Functor ((<&>))
+import Data.Text (Text) 
+import Prelude hiding (readFile)
+import Data.ByteString(readFile)
 
-fileRead :: String -> IO String
+fileRead :: FilePath -> IO String
 fileRead fileName = do
   h <- openFile fileName ReadMode
   hSetEncoding h utf8
   hGetContents' h
 
-fileWrite :: String -> String -> IO ()
+fileWrite :: FilePath -> String -> IO ()
 fileWrite fileName str = do
   h <- openFile fileName WriteMode
   hSetEncoding h utf8
   hPutStr h str
   hClose h
+
+fileRead2 :: FilePath -> IO Text
+fileRead2 fileName = readFile fileName <&> decodeUtf8
